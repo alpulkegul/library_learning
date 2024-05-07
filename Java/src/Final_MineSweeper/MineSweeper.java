b6de014776ab18a5b52e3c7f3e6fb8ce8d26d8f8
alpulkegul <alpulkegul@gmail.com>
2024-05-07 06:24:42 +0300

    Final Project

--- Java/src/Final_MineSweeper/MineSweeper.java
+++ Java/src/Final_MineSweeper/MineSweeper.java
@@ -1,108 +1,147 @@
 package Final_MineSweeper;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class MineSweeper {
-    int rowNumb, colNumb, size;
-    int[][] map, board;
-    boolean isGame = true;
-    Random rnd = new Random();
-    Scanner inp = new Scanner(System.in);
+public class MineSweeper{
+    private final Scanner inp = new Scanner(System.in);
+    private final Random rand = new Random();
+    private int row;
+    private int col;
+    private String[][] secretMap;
+    private String[][] visibleMap;
+    private int mineCount;
+    private String userName;
 
-    MineSweeper(int rowNumb, int colNumb) {
-        this.rowNumb = rowNumb;
-        this.colNumb = colNumb;
-        this.map = new int[rowNumb][colNumb];
-        this.board = new int[rowNumb][colNumb];
-        this.size = rowNumb*colNumb;
-    }
+    public void go() {
+        System.out.println("Oyuna başlamadan önce sana nasıl hitap etmemizi istersin.");
+        System.out.print("Kullanıcı Adı: ");
+        userName = inp.next();
+        System.out.println("Merhaba " + userName + ",");
+        System.out.println("Mayın Tarlası oyununa hoş geldin.\n" +
+                             "Öncelikle, parselin boyutu kaça kaç (örnek: 4x5)olmasını istersin?");
+        System.out.print("Oluşacak parselin satır sayısı: ");
+        row = inp.nextInt();
+        System.out.print("Oluşacak parselin sütun sayısı: ");
+        col = inp.nextInt();
+        System.out.println("Tarlan hazırlanıyor.. Geri Sayım başlasın!");
+        System.out.println("    " + 3);
+        System.out.println("    " + 2);
+        System.out.println("    " + 1);
+        System.out.println("İşte hazır!");
 
-    public void run() {
-        int row, col, success=0;
-        prepareGame();
-        print(map);
-        System.out.println("Game is On!");
-        while (isGame) {
-            print(board);
-            System.out.print("Row: ");
-            row = inp.nextInt();
-            System.out.print("Column: ");
-            col = inp.nextInt();
-            if (row < 2 && col < 2) {
-                System.out.println("You have to enter at least 2x2");
-            }
 
-            if (row < 0 || row >= rowNumb) {
+        mineCount = (row * col) / 4;
 
-                System.out.println("Coordinate error!");
-                continue;
-            }
-            if (col < 0 || col >= colNumb) {
-                System.out.println("Coordinate error!");
-                continue;
-            }
-            if (map[row][col] != -1) {
-                check(row, col);
-                success++;
-                if (success == (size - (size / 4))) {
-                    System.out.println("Win! ");
-                    break;
-                }
-            } else {
-                isGame= false;
-                System.out.println("Game Over!");
+        secretMap = new String[row][col];
+        visibleMap = new String[row][col];
+
+        for (int i = 0; i < secretMap.length; i++) {
+            for (int j = 0; j < secretMap[i].length; j++) {
+                secretMap[i][j] = "-";
+                visibleMap[i][j] = "-";
             }
         }
-
-    }
-
-    public void check(int r, int c) {
-        if (board[r][c] == 0) {
-            if ((c < colNumb - 1) && (map[r][c + 1] == -1)) {
-                board[r][c]++;
-
-            }
-            if ((r < rowNumb - 1) && (map[r + 1][c] == -1)) {
-                board[r][c]++;
-
-            }
-            if ((r > 0) && (map[r - 1][c] == -1)) {
-                board[r][c]++;
-            }
-            if ((c > 0) && (map[r][c - 1] == -1)) {
-                board[r][c]++;
-            }
-            if (board[r][c] == 0) {
-                board[r][c] = -2;
+        while (mineCount > 0) {
+            int i = rand.nextInt(row);
+            int j = rand.nextInt(col);
+            if (secretMap[i][j].equals("-")) {
+                secretMap[i][j] = "*";
+                mineCount--;
             }
         }
+        printMapVisible();
 
-    }
-    public void prepareGame() {
-        int randRow, randCol, count = 0;
-        while(count != (size/4)) {
-            randRow = rnd.nextInt(rowNumb);
-            randCol = rnd.nextInt(colNumb);
-            if (map[randRow][randCol] != -1) {
-                map[randRow][randCol] = -1;
-                count++;
-            }
-        }
+        play();
 
     }
 
 
-
-    public void print(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                if (arr[i][j] >= 0 ) {
-                    System.out.print(" ");
-                }
-                System.out.print(arr[i][j] + " ");
+    private void printMapVisible(){
+        for (int i = 0; i < visibleMap.length; i++) {
+            for (int j = 0; j < visibleMap[i].length; j++) {
+                System.out.print(visibleMap[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+    private void printMapSecret() {
+        for (int i = 0; i < secretMap.length; i++) {
+            for (int j = 0; j < secretMap[i].length; j++) {
+                System.out.print(secretMap[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void play(){
+        boolean finish = true;
+        while (finish){
+            System.out.print("Satır gir:");
+            int slcRow = inp.nextInt();
+            System.out.print("Sütun gir:");
+            int slcCol = inp.nextInt();
+
+            int n = 0;
+
+            if (slcRow < row && slcCol < col) {
+                if (secretMap[slcRow][slcCol].equals("-") && visibleMap[slcRow][slcCol].equals("-")) {
+                    for (int i = slcRow - 1; i < slcRow; i++) {
+                        for (int j = slcCol - 1; j < slcCol; i++) {
+                            if (i >= 0 && j >= 0 && i < row && j < col && secretMap[i][j].equals("*")) {
+                                n++;
+                                visibleMap[slcRow][slcCol] = Integer.toString(n);
+                            } else {
+                                visibleMap[slcRow][slcCol] = Integer.toString(n);
+                            }
+                        }
+                    }
+                    printMapVisible();
+
+                    if (!checkWin()) {
+                        System.out.println("Tebrikler. Kazandın!");
+                        printMapSecret();
+                        finish = false;
+
+                    }
+                } else if (secretMap[slcRow][slcCol].equals("*")) {
+                    System.out.println("Yazık oldu, mayına bastın :/");
+                    printMapSecret();
+                } else if (!visibleMap[slcRow][slcCol].equals("-")) {
+                    System.out.println("Bu koordinat zaten seçildi. Başka koordinat seç.");
+                }
+
+            }else {
+                System.out.println("Sınırını bil! Alan dışından seçim yaptın.");
+            }
+
+
+
+        }
+
+
+    }
+
+    private boolean checkWin() {
+        int count = 0;
+        int mineNumb = 0;
+        for (int i = 0; i < visibleMap.length; i++) {
+            for (int j = 0; j < visibleMap[i].length; j++) {
+                if (visibleMap[i][j].equals("-")) {
+                    count++;
+                }
+                if (secretMap[i][j].equals("*")) {
+                    mineNumb++;
+                }
+            }
+        }
+        if (mineNumb == count) {
+            return true;
+        }
+        return false;
+    }
+
 }
 
--- Java/src/Final_MineSweeper/main.java
+++ Java/src/Final_MineSweeper/main.java
@@ -4,16 +4,9 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        int row, col;
-        System.out.println("Welcome to Mine Sweeper!");
-        System.out.println("Enter the sizes you want to play with ↓\n");
-        System.out.print("Number of rows: ");
-        row = inp.nextInt();
-        System.out.print("Number of columns: ");
-        col = inp.nextInt();
-        MineSweeper mine = new MineSweeper(row, col);
-        mine.run();
-    }
+        MineSweeper a = new MineSweeper();
 
+        a.go();
+
+    }
 }
